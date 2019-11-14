4c9c2cbee78899d81c45a8ead16396b444bd86d2
Minsung89 <minsung0217@gmail.com>
2019-11-14 21:50:50 +0900

    회원가입 입력화면 구현

---
+++ app/src/main/java/com/tjoeum/a20191114_01_okhttp/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.tjoeum.a20191114_01_okhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
